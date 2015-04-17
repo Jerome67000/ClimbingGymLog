d1c6856dc66abf96b7daee79c545dbd36288e855
unknown <rcdsm06@pole410-R.SYSINFO>
2015-04-17 14:33:28 +0200

    close #17, Ajout de EditVoieDialog permetant de modifier une voie en la touchant

--- app/src/main/java/fr/jerome/climbinggymlog/data/VoieDB.java
+++ app/src/main/java/fr/jerome/climbinggymlog/data/VoieDB.java
@@ -3,6 +3,7 @@ package fr.jerome.climbinggymlog.data;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -10,6 +11,8 @@ import java.util.List;
 
 import fr.jerome.climbinggymlog.helpers.AppManager;
 import fr.jerome.climbinggymlog.models.Cotation;
+import fr.jerome.climbinggymlog.models.StyleVoie;
+import fr.jerome.climbinggymlog.models.TypeEsc;
 import fr.jerome.climbinggymlog.models.Voie;
 
 /**
@@ -22,8 +25,8 @@ public class VoieDB extends DBHandler {
     public static final String ID = "_id";
     public static final String NOM = "nom";
     public static final String COTATION = "cotation_id";
-    public static final String TYPE_ESCALADE = "type_escalade_id";
-    public static final String STYLE = "style_voie_id";
+    public static final String ID_TYPE_ESCALADE = "type_escalade_id";
+    public static final String ID_STYLE_VOIE = "style_voie_id";
     public static final String REUSSIE = "reussie";
     public static final String A_VUE = "a_vue";
     public static final String NOTE = "note";
@@ -32,10 +35,10 @@ public class VoieDB extends DBHandler {
 
     public static final String  CREATE_TABLE =  "CREATE TABLE " + TABLE_NAME + "(" +
                                 ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            NOM + " TEXT, " +
-                                COTATION + " INTEGER, " +
-                                TYPE_ESCALADE + " INTEGER, " +
-                                STYLE + " INTEGER, " +
+                                NOM + " TEXT, " +
+                                                    COTATION + " INTEGER, " +
+                                ID_TYPE_ESCALADE + " INTEGER, " +
+                                ID_STYLE_VOIE + " INTEGER, " +
                                 REUSSIE + " BOOL, " +
                                 A_VUE + " BOOL, " +
                                 NOTE + " TEXT, " +
@@ -47,6 +50,84 @@ public class VoieDB extends DBHandler {
     public VoieDB(Context context) {
 
         super(context);
+    }
+
+    /**
+     * @param voie la voie inserée dans la table
+     */
+    public Voie insert(Voie voie) {
+
+        ContentValues value = new ContentValues();
+
+        value.put(NOM, voie.getNom());
+        value.put(COTATION, voie.getCotation().getId());
+        value.put(ID_TYPE_ESCALADE, voie.getTypeEscalade().getId());
+        value.put(ID_STYLE_VOIE, voie.getStyle().getId());
+        value.put(REUSSIE, voie.isReussi());
+        value.put(A_VUE, voie.isAVue());
+        value.put(NOTE, voie.getNote());
+        value.put(ID_SEANCE_VOIE, voie.getIdSeance());
+
+        // récupération de l'id pour le setter dans l'objet
+        long insertId =  database.insert(TABLE_NAME, null, value);
+        voie.setId(insertId);
+        Log.d("SQL", "Ajout de la voie " + voie.getNom() + " id : " + voie.getId() + " à la table Voie");
+
+        return voie;
+    }
+
+    public Voie select(int id) {
+
+        Cursor c = database.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + ID + "=?", new String[]{String.valueOf(id)});
+
+        c.moveToFirst();
+
+        Log.d("C.tostrin", android.database.DatabaseUtils.dumpCursorToString(c));
+
+        boolean isReussi = c.getInt(5) > 0;
+        boolean isAVue = c.getInt(6) > 0;
+
+        // - 1 car l'index d'une list commence à zéro et l'id à 1
+        Cotation cot = AppManager.cotations.get(c.getInt(2) - 1);
+        TypeEsc typeEsc = AppManager.typesEsc.get(c.getInt(3) - 1);
+        StyleVoie styleVoie = AppManager.styleVoies.get(c.getInt(4) - 1);
+
+        Voie voie = new Voie(c.getLong(0), c.getString(1), cot, typeEsc, styleVoie, isReussi, isAVue, c.getString(7), c.getLong(8));
+        c.close();
+
+        return voie;
+    }
+
+    /**
+     * @param voie la voie modifiée
+     */
+    public void update(Voie voie) {
+
+        ContentValues value = new ContentValues();
+
+        value.put(NOM, voie.getNom());
+        value.put(COTATION, voie.getCotation().getId());
+        value.put(ID_TYPE_ESCALADE, voie.getTypeEscalade().getId());
+        value.put(ID_STYLE_VOIE, voie.getStyle().getId());
+        value.put(REUSSIE, voie.isReussi());
+        value.put(A_VUE, voie.isAVue());
+        value.put(NOTE, voie.getNote());
+
+        database.update(TABLE_NAME, value, ID + "=" + voie.getId(), null);
+
+        Log.d("SQL", "update de la voie " + voie.toString());
+    }
+
+
+    /**
+     * @param voie séance à supprimer
+     */
+    public void delete(Voie voie) {
+
+        long id = voie.getId();
+        database.delete(TABLE_NAME, ID + " = " + id, null);
+
+        Log.d("SQL", "suppression de la voie " + voie.getNom() + " id : " + voie.getId() + " de la table Voie");
     }
 
     /**
@@ -64,67 +145,15 @@ public class VoieDB extends DBHandler {
             boolean isReussi = c.getInt(5) > 0;
             boolean isAVue = c.getInt(6) > 0;
 
-            // l'id de la cotation - 1 car l'index d'une list commence à zéro
-            Cotation cot = AppManager.cotations.get(c.getInt(2)-1);
+            Cotation cot = AppManager.cotations.get(c.getInt(2) - 1);
+            TypeEsc typeEsc = AppManager.typesEsc.get(c.getInt(3) - 1);
+            StyleVoie styleVoie = AppManager.styleVoies.get(c.getInt(4) - 1);
 
-            Voie voie = new Voie(c.getLong(0), c.getString(1), cot, AppManager.typesEsc.get(c.getInt(3)),
-                    AppManager.styleVoies.get(c.getInt(4)), isReussi, isAVue, c.getString(7), c.getLong(8));
+            Voie voie = new Voie(c.getLong(0), c.getString(1), cot, typeEsc, styleVoie, isReussi, isAVue, c.getString(7), c.getLong(8));
             voies.add(voie);
             c.moveToNext();
         }
         c.close();
         return voies;
-    }
-
-    /**
-     * @param voie la voie inserée dans la table
-     */
-    public Voie insert(Voie voie) {
-
-        ContentValues value = new ContentValues();
-
-        value.put(NOM, voie.getNom());
-        value.put(COTATION, voie.getCotation().getId());
-        value.put(TYPE_ESCALADE, voie.getTypeEscalade().getType());
-        value.put(STYLE, voie.getStyle().getStyle());
-        value.put(REUSSIE, voie.isReussi());
-        value.put(A_VUE, voie.isAVue());
-        value.put(NOTE, voie.getNote());
-        value.put(ID_SEANCE_VOIE, voie.getIdSeance());
-
-        // récupération de l'id pour le setter dans l'objet
-        long insertId =  database.insert(TABLE_NAME, null, value);
-        voie.setId(insertId);
-        Log.d("SQL", "Ajout de la voie " + voie.getNom() + " id : " + voie.getId() + " à la table Voie");
-
-        return voie;
-    }
-
-    /**
-     * @param voie la séance à récupérer dans la base
-     * @return The cursor of the seance
-     */
-    public Cursor select(Voie voie) {
-
-        long id = voie.getId();
-        return database.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + ID + "=?", new String[]{String.valueOf(id)});
-    }
-
-    /**
-     * @param voie la voie modifiée
-     */
-    public void update(Voie voie) {
-
-    }
-
-    /**
-     * @param voie séance à supprimer
-     */
-    public void delete(Voie voie) {
-
-        long id = voie.getId();
-        database.delete(TABLE_NAME, ID + " = " + id, null);
-
-        Log.d("SQL", "suppression de la voie " + voie.getNom() + " id : " +voie.getId() + " de la table Voie");
     }
 }

fdba5215ac9b9232f34bc1b0fda6415bc0d5d47c
Jerome67000 <jerome.gully0@gmaail.com>
2015-05-03 21:14:17 +0200

    ref #3, mise en place de la PreferenceActivity, les vues de detail reste a creer

--- app/src/main/java/fr/jerome/climbinggymlog/activities/MainActivity.java
+++ app/src/main/java/fr/jerome/climbinggymlog/activities/MainActivity.java
@@ -10,6 +10,9 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -152,25 +155,26 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent i = new Intent(this, PreferencesActivity.class);
+            this.startActivity(i);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
---
+++ app/src/main/java/fr/jerome/climbinggymlog/activities/PreferencesActivity.java
@@ -0,0 +1,49 @@
+package fr.jerome.climbinggymlog.activities;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import fr.jerome.climbinggymlog.R;
+
+/**
+ * Created by jerome on 03/05/15.
+ */
+public class PreferencesActivity extends PreferenceActivity {
+
+    private Toolbar toolBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        prepareLayout();
+        addPreferencesFromResource(R.xml.preferences);
+        toolBar.setTitle(getString(R.string.title_activity_preferences));
+    }
+
+    // Permetant d'avoir la Toolbar dans une PreferenceActivity
+    private void prepareLayout() {
+        ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+        View content = root.getChildAt(0);
+        LinearLayout toolbarContainer = (LinearLayout) View.inflate(this, R.layout.activity_preferences, null);
+
+        root.removeAllViews();
+        toolbarContainer.addView(content);
+        root.addView(toolbarContainer);
+
+        toolBar = (Toolbar) toolbarContainer.findViewById(R.id.toolbar_pref);
+        toolBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.main_color)));
+        toolBar.setNavigationIcon(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        toolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}

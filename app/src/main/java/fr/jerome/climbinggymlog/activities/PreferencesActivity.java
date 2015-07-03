5793658f49adec6bd439ea40797e93c1c479e4ba
Jerome67000 <jerome.gully0@gmaail.com>
2015-07-04 01:05:59 +0200

    close #8, licences des librairies tierces ajoutees dans les parametres de l'app

--- app/src/main/java/fr/jerome/climbinggymlog/activities/PreferencesActivity.java
+++ app/src/main/java/fr/jerome/climbinggymlog/activities/PreferencesActivity.java
@@ -3,12 +3,22 @@ package fr.jerome.climbinggymlog.activities;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import de.psdev.licensesdialog.LicensesDialog;
+import de.psdev.licensesdialog.LicensesDialogFragment;
+import de.psdev.licensesdialog.licenses.ApacheSoftwareLicense20;
+import de.psdev.licensesdialog.licenses.GnuLesserGeneralPublicLicense21;
+import de.psdev.licensesdialog.licenses.License;
+import de.psdev.licensesdialog.model.Notice;
+import de.psdev.licensesdialog.model.Notices;
 import fr.jerome.climbinggymlog.R;
 
 /**
@@ -47,5 +57,41 @@ public class PreferencesActivity extends PreferenceActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        switch (preference.getKey()) {
+            case "pref_key_interface_def_val" : {
+
+            } break;
+            case "pref_key_notifications_new_event" : {
+
+            } break;
+            case "pref_key_notifications_event_imm" : {
+
+            } break;
+            case "pref_key_compte_modifier" : {
+
+            } break;
+            case "pref_key_compte_deco" : {
+
+            } break;
+            case "pref_key_infoleg_licence" : {
+                showLicences();
+            } break;
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    public void showLicences() {
+        final Notices notices = new Notices();
+        notices.addNotice(new Notice("MPAndroidChart", "https://github.com/PhilJay/MPAndroidChart", "Copyright 2015 Philipp Jahoda", new ApacheSoftwareLicense20()));
+
+        new LicensesDialog.Builder(this)
+                .setNotices(notices)
+                .setIncludeOwnLicense(true)
+                .build()
+                .show();
     }
 }
